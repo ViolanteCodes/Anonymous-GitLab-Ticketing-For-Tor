038b505b640462e05a58c7677b086ac1a04f2c38
ViolanteCodes <maria@mariadong.com>
2020-12-09 17:02:00 -0500

    added user_identifier path <str:user_identifier>/

--- anonticket/urls.py
+++ anonticket/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="anonticket/index.html"), name='home'),
+    path('<str:user_identifier>/', views.search_by_id, name='user-identifier'),
     path('search_by_id/', views.search_by_id, name="search-by-id"),
     path('create_issue/', views.create_issue, name="create-issue"),
     path('save_issue/', views.save_issue, name="save-issue"),
