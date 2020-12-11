5cc4da0cac145b5b22f84690f5a7bf61d3357734
ViolanteCodes <maria@mariadong.com>
2020-12-11 13:07:14 -0500

    Added view/form/url to create an issue for a "logged in" user

--- anonticket/urls.py
+++ anonticket/urls.py
@@ -8,9 +8,8 @@ from anonticket.views import CreateIdentifierView, UserLandingView
 urlpatterns = [
     path('', TemplateView.as_view(template_name="anonticket/index.html"), name='home'),
     path('search_by_id/', views.search_by_id, name="search-by-id"),
-    path('create_issue/', views.create_issue, name="create-issue"),
-    path('save_issue/', views.save_issue, name="save-issue"),
     path('user/create_identifier/', views.CreateIdentifierView.as_view(), name='create-identifier'),
-    path('user/login/', views.login_with_codename, name='login'),
+    path('user/login/', views.login_with_codename, name='login'),    
+    path('user/<str:user_identifier>/create_issue/', views.create_issue, name='create-issue'),
     path('user/<str:user_identifier>/', views.UserLandingView.as_view(), name='user-landing'),
 ]
