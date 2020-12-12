d4a0a661757267f82e919c9485600aa8bc994ae5
ViolanteCodes <maria@mariadong.com>
2020-12-11 22:10:48 -0500

    converted user_login CBV to function-based -view, so that validation function could be pulled in more easily. tested code-name validator, working correctly in runserver

--- anonticket/urls.py
+++ anonticket/urls.py
@@ -2,7 +2,11 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.views.generic import TemplateView
-from anonticket.views import CreateIdentifierView, UserLandingView, IssueSuccessView
+from anonticket.views import (
+    CreateIdentifierView, 
+    IssueSuccessView,
+    UserLoginErrorView
+    )
 
 
 urlpatterns = [
@@ -10,7 +14,8 @@ urlpatterns = [
     path('search_by_id/', views.search_by_id, name="search-by-id"),
     path('user/create_identifier/', views.CreateIdentifierView.as_view(), name='create-identifier'),
     path('user/login/', views.login_with_codename, name='login'),
+    path('user/<str:user_identifier>/login_error/', views.UserLoginErrorView.as_view(), name='user-login-error'),
     path('user/<str:user_identifier>/create_issue/success', views.IssueSuccessView.as_view(), name='issue-created'),        
     path('user/<str:user_identifier>/create_issue/', views.create_issue, name='create-issue'),
-    path('user/<str:user_identifier>/', views.UserLandingView.as_view(), name='user-landing'),
+    path('user/<str:user_identifier>/', views.user_landing_view, name='user-landing'),
 ]
