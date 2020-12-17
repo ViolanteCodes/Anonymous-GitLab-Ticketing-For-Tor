75f5a032dbf5409759f03ce2f87c358758ff015e
ViolanteCodes <maria@mariadong.com>
2020-12-17 11:46:45 -0500

    year only function tested good

--- shared/templatetags/custom_filters.py
+++ shared/templatetags/custom_filters.py
@@ -2,14 +2,21 @@
 # the following function calls are for custom filters.
 
 from django import template
+from django.template.defaultfilters import stringfilter
 register = template.Library()
 
 # Custom filters go here.
-def pretty_date(string):
-    """Takes an ISO 8601 Date-Time String and returns it in a pretty date."""
+
+# Register filter with library and define it as a function that only 
+# takes strings to avoid AttributeErrors
+
+@register.filter(is_safe=True)
+@stringfilter
+def pretty_date(iso_string):
+    """Takes an ISO 8601 Date-Time string and returns it in a pretty date format."""
     try:
         pretty_date = ''
-        year = f"{string[0]}{string[1]}{string[2]}{string[3]}"
+        year = f"{iso_string[0]}{iso_string[1]}{iso_string[2]}{iso_string[3]}"
         pretty_date = year
         return pretty_date
     except:
