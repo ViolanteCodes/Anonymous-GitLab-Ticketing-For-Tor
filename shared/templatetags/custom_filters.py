7760b0ddae46ea43a5c853a41cde6fe97acc0f42
ViolanteCodes <maria@mariadong.com>
2020-12-17 11:57:18 -0500

    adjusted pretty_datetime fitler variable names

--- shared/templatetags/custom_filters.py
+++ shared/templatetags/custom_filters.py
@@ -12,16 +12,16 @@ register = template.Library()
 
 @register.filter(is_safe=True)
 @stringfilter
-def pretty_date(iso_string):
+def pretty_datetime(iso_string):
     """Takes an ISO 8601 Date-Time string and returns it in a pretty date format."""
     try:
-        pretty_date = ''
+        pretty_datetime = ''
         year = f"{iso_string[0]}{iso_string[1]}{iso_string[2]}{iso_string[3]}"
         month = f"{iso_string[5]}{iso_string[6]}"
         day = f"{iso_string[8]}{iso_string[9]}"
         time = f"{iso_string[11]}{iso_string[12]}{iso_string[13]}{iso_string[14]}{iso_string[15]}"
-        pretty_date = f"{month}/{day}/{year} - {time} UTC"
-        return pretty_date
+        pretty_datetime = f"{month}/{day}/{year} - {time} UTC"
+        return pretty_datetime
     except:
         pass
 
