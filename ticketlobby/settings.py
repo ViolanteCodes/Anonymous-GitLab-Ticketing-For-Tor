60d2d05f241ec90b5f2e53df41d34dc8a2798284
ViolanteCodes <maria@mariadong.com>
2020-12-18 12:28:47 -0500

    updated readme and added markdownify to settings.py

--- ticketlobby/settings.py
+++ ticketlobby/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'markdownify',
     'shared.apps.SharedConfig',
     'anonticket.apps.AnonticketConfig',
 ]
