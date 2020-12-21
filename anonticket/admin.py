b5667fbcb716cfcf6d6084b5fa57f4e85c477519
ViolanteCodes <maria@mariadong.com>
2020-12-21 16:06:03 -0500

    Added a list display for issues so that admins can see which are approved and what projects issues are assigned to

--- anonticket/admin.py
+++ anonticket/admin.py
@@ -4,5 +4,8 @@ from django.contrib import admin
 
 from .models import Project, Issue, UserIdentifier
 admin.site.register(Project)
-admin.site.register(Issue)
 admin.site.register(UserIdentifier)
+
+@admin.register(Issue)
+class IssueModelAdmin(admin.ModelAdmin):
+    list_display = ('issue_title', 'linked_project','reviewer_status')
