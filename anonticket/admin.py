6063fb9f3d306863c70cf40c68023946087f116a
ViolanteCodes <maria@mariadong.com>
2020-12-21 17:08:38 -0500

    added moderator group and permissions (Issue all, Project/UserIdentifier view only)

--- anonticket/admin.py
+++ anonticket/admin.py
@@ -6,7 +6,16 @@ from .models import Project, Issue, UserIdentifier
 admin.site.register(Project)
 admin.site.register(UserIdentifier)
 
+def bulk_approve_issues(modeladmin, request, queryset):
+    """Add a bulk approval method for issues to admin panel."""
+    for issue in queryset:
+        issue.reviewer_status = 'A'
+        issue.save()
+
+bulk_approve_issues.short_description = "Approve selected issues and post to GitLab."
+
 @admin.register(Issue)
 class IssueModelAdmin(admin.ModelAdmin):
     list_display = ('issue_title', 'linked_project','reviewer_status')
     list_filter = ('reviewer_status', )
+    actions = [bulk_approve_issues]
