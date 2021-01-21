76cc63ca49290e5ed65caeb5ff0739f7c377e819
ViolanteCodes <maria@mariadong.com>
2021-01-21 16:42:00 -0500

    added ModeratorGitlabAccountRequestUpdateView to urls.py

--- anonticket/urls.py
+++ anonticket/urls.py
@@ -13,25 +13,69 @@ from anonticket.views import (
     NoteCreateView,
     ModeratorNoteUpdateView,
     ModeratorIssueUpdateView,
+    ModeratorGitlabAccountRequestUpdateView,
     )
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="anonticket/index.html"), name='home'),
-    path('user/create_identifier/', views.CreateIdentifierView.as_view(), name='create-identifier'),
-    path('user/login/', views.login_view, name='login'),
-    path('user/<str:user_identifier>/login_error/', views.UserLoginErrorView.as_view(), name='user-login-error'),
-    path('user/<str:user_identifier>/projects/<slug:project>/issues/<int:issue_iid>/notes/create/', views.NoteCreateView.as_view(), name='create-note'),
-    path('user/<str:user_identifier>/projects/<slug:project>/issues/<int:issue_iid>/notes/<int:pk>/', views.PendingNoteDetailView.as_view(), name='pending-note'),
-    path('user/<str:user_identifier>/projects/<slug:project_slug>/issues/<int:gitlab_iid>/details/', views.issue_detail_view, name='issue-detail-view'),
-    path('user/<str:user_identifier>/projects/<slug:project_slug>/issues/pending/<int:pk>/', views.PendingIssueDetailView.as_view(), name='pending-issue-detail-view'),
-    path('user/<str:user_identifier>/projects/all/issues/search/', views.issue_search_view, name="issue-search"),
-    path('user/<str:user_identifier>/projects/<slug:slug>/', views.ProjectDetailView.as_view(), name='project-detail'),
-    path('user/<str:user_identifier>/projects/', views.ProjectListView.as_view(), name='project-list'),
-    path('user/<str:user_identifier>/create/success/', views.IssueSuccessView.as_view(), name='issue-created'),        
-    path('user/<str:user_identifier>/create_issue/', views.create_issue_view, name='create-issue'),
-    path('user/<str:user_identifier>/', views.user_landing_view, name='user-landing'),
-    path('moderator/', views.moderator_view, name='moderator'),
-    path('moderator/update_note/<int:pk>', views.ModeratorNoteUpdateView.as_view(), name='mod-update-note'),
-    path('moderator/update_issue/<int:pk>', views.ModeratorIssueUpdateView.as_view(), name='mod-update-issue'),
+    path(
+        '', 
+        TemplateView.as_view(template_name="anonticket/index.html"), 
+        name='home'),
+    path(
+        'user/create_identifier/', 
+        views.CreateIdentifierView.as_view(), 
+        name='create-identifier'),
+    path(
+        'user/login/', 
+        views.login_view, name='login'),
+    path(
+        'user/<str:user_identifier>/login_error/', 
+        views.UserLoginErrorView.as_view(), name='user-login-error'),
+    path(
+        'user/<str:user_identifier>/projects/<slug:project>/issues/<int:issue_iid>/notes/create/', 
+        views.NoteCreateView.as_view(), 
+        name='create-note'),
+    path(
+        'user/<str:user_identifier>/projects/<slug:project>/issues/<int:issue_iid>/notes/<int:pk>/', 
+        views.PendingNoteDetailView.as_view(), name='pending-note'),
+    path(
+        'user/<str:user_identifier>/projects/<slug:project_slug>/issues/<int:gitlab_iid>/details/', 
+        views.issue_detail_view, name='issue-detail-view'),
+    path(
+        'user/<str:user_identifier>/projects/<slug:project_slug>/issues/pending/<int:pk>/', 
+        views.PendingIssueDetailView.as_view(), name='pending-issue-detail-view'),
+    path(
+        'user/<str:user_identifier>/projects/all/issues/search/', 
+        views.issue_search_view, name="issue-search"),
+    path(
+        'user/<str:user_identifier>/projects/<slug:slug>/', 
+        views.ProjectDetailView.as_view(), name='project-detail'),
+    path(
+        'user/<str:user_identifier>/projects/', 
+        views.ProjectListView.as_view(), name='project-list'),
+    path(
+        'user/<str:user_identifier>/create/success/', 
+        views.IssueSuccessView.as_view(), name='issue-created'),        
+    path(
+        'user/<str:user_identifier>/create_issue/', 
+        views.create_issue_view, name='create-issue'),
+    path(
+        'user/<str:user_identifier>/', 
+        views.user_landing_view, name='user-landing'),
+    path(
+        'moderator/', 
+        views.moderator_view, name='moderator'),
+    path(
+        'moderator/update-note/<int:pk>', 
+        views.ModeratorNoteUpdateView.as_view(), 
+        name='mod-update-note'),
+    path(
+        'moderator/update-issue/<int:pk>', 
+        views.ModeratorIssueUpdateView.as_view(), 
+        name='mod-update-issue'),
+    path(
+        'moderator/update-gitlab-account-request/<int:pk>', 
+        views.ModeratorGitlabAccountRequestUpdateView.as_view(), 
+        name='mod-update-issue'),
 ]
