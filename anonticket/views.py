b1058927b221f8ddbc702fb092b21d37ee764405
ViolanteCodes <maria@mariadong.com>
2021-02-08 12:47:15 -0500

    refactored ProjectDetailView get_context_data to group similar functions

--- anonticket/views.py
+++ anonticket/views.py
@@ -300,30 +300,30 @@ class ProjectDetailView(DetailView):
     model = Project
 
     def get_context_data(self, **kwargs):
-        # Grab kwargs          
+        # Grab variables from kwargs          
         context = super().get_context_data(**kwargs)
-        # set user_identifier for results dict.
         user_identifier = self.kwargs['user_identifier']
-        context['results'] = {'user_identifier': user_identifier}
-        # Fetch the working project from the database or 404                    
+        page_number = self.kwargs['page_number']
         project_slug = self.kwargs['slug']
+        # Fetch the project from the database                   
         db_project = Project.objects.get(
             slug=project_slug
         )
-        # Get the page_number to paginate the GL API call.
-        page_number = self.kwargs['page_number']
-        context['page_number'] = page_number
-        # Grab the gitlab ID from database and create the project.
+        # Grab the gitlab ID from db and create GL project object with
+        # a lazy API call.
         gitlab_id = db_project.gitlab_id
         gl_project = gitlab_get_project(gitlab_id, lazy=True)
         # Save the project attributes to context dict.
+        context['results'] = {'user_identifier': user_identifier}
+        context['page_number'] = page_number
         context['gitlab_project'] = gl_project.attributes
+        # Get the open issues and save to dict.
         context['open_issues']={}
         check_open = self.get_pagination(
             user_identifier, project_slug, gl_project, page_number, issue_state='opened')
         for key, value in check_open.items():
             context['open_issues'][key] = value
-        # Get closed issues
+        # Get the closed issues aqnd save to dict.
         context['closed_issues'] = {}
         check_closed = self.get_pagination(
             user_identifier, project_slug, gl_project, page_number, issue_state='closed'
