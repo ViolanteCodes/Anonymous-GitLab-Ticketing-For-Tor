4f6512483bc639f2295fa87b25a6f445a68b6571
ViolanteCodes <maria@mariadong.com>
2021-02-10 14:10:50 -0500

    enabled local memory cache in django backend

--- ticketlobby/settings.py
+++ ticketlobby/settings.py
@@ -87,6 +87,13 @@ DATABASES = {
     }
 }
 
+# Caching - necessary for Django-Ratelimit
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
