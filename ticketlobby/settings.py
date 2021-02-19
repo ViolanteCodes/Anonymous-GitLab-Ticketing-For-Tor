ee691e665456f66be74c0dc14d061905d3c125bb
ViolanteCodes <maria@mviolante.com>
2021-02-19 13:20:04 -0500

    removed import gitlab statement from settings.py

--- ticketlobby/settings.py
+++ ticketlobby/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 from decouple import config
 from django.core.exceptions import ImproperlyConfigured
-import gitlab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
