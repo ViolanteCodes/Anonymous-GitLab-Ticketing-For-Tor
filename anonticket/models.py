d3bed0c442227a1734ddfe2b24b78a9c7a01c9aa
ViolanteCodes <maria@mviolante.com>
2021-02-23 10:53:18 -0500

    added a unique = True call to emailfield

--- anonticket/models.py
+++ anonticket/models.py
@@ -231,7 +231,9 @@ class GitlabAccountRequest(models.Model):
         hyphens(-).""",
         validators=[check_if_user_in_gitlab]
         )
-    email = models.EmailField()
+    email = models.EmailField(
+        unique = True,
+        )
     reason = models.CharField(
         max_length=256, 
         help_text = """Please explain why you want to collaborate with the 
